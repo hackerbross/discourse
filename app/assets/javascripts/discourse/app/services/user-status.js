cfcb0716fafa07eb014b875ed9e8e20bcbd7d83e
Andrei Prigorshnev <a.prigorshnev@gmail.com>
2022-06-10 20:12:30 +0400

    CLIENT: handle empty ends_at value

--- app/assets/javascripts/discourse/app/services/user-status.js
+++ app/assets/javascripts/discourse/app/services/user-status.js
@@ -8,7 +8,7 @@ export default class UserStatusService extends Service {
     const data = {
       emoji: status.emoji,
       description: status.description,
-      ends_at: status.endsAt.toISOString(),
+      ends_at: status.endsAt?.toISOString(),
     };
 
     await ajax({
